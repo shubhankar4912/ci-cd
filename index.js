bf6d295ce644823535fff19425255bbb3bb29041
shubhankar4912 <97121506+shubhankar4912@users.noreply.github.com>
2024-01-14 11:09:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 4000;
+const port = 5000;
 
 app.get('/', (req, res) => {
     res.send('Hello, this is a simple Express server!');
